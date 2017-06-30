d43ed6d1d48b434be776a55e7374082a74517121
rajasekarrajendran <r.rajasekarrajendran@yahoo.com>
2017-06-30 18:22:47 +0530

    my first commit

---
+++ banner-images/bannercontroller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('MyApp')
+        .controller('bannerController', bannerController);
+
+    bannerController.$inject = ['$location', '$http', '$state', '$window','$scope' , '$document'];
+    function bannerController($location, $http, $state ,  $window, $scope, $document) {
+        
+        
+  
+
+}
+})();
